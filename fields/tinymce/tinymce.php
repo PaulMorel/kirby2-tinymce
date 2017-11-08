c475816ff14a3cd6f5de7d4e8f661c8d5ac40c45
Paul Morel <paul.morel@gmail.com>
2017-11-07 20:57:27 -0500

    Custom TinyMCE build

--- fields/tinymce/tinymce.php
+++ fields/tinymce/tinymce.php
@@ -20,30 +20,28 @@ class TinyMCEField extends BaseField
 
     public function input() {
         $input = new Brick('textarea', false);
-        $input->addClass('input');
-//        $input->attr([
-//            'type'         => $this->type(),
-//            'value'        => '',
-//            'required'     => $this->required(),
-//            'name'         => $this->name(),
-//            'autocomplete' => $this->autocomplete() === false ? 'off' : 'on',
-//            'autofocus'    => $this->autofocus(),
-//            'placeholder'  => $this->i18n($this->placeholder()),
-//            'readonly'     => $this->readonly(),
-//            'disabled'     => $this->disabled(),
-//            'id'           => $this->id()
-//        ]);
-//        if ( ! is_array($this->value()) ) {
-//            $input->val($this->value());
-//        }
-//        if ( $this->readonly() ) {
-//            $input->attr('tabindex', '-1');
-//            $input->addClass('input-is-readonly');
-//        }
+        $input->addClass('field-tinymce');
+        $input->attr([
+            'required'     => $this->required(),
+            'name'         => $this->name(),
+            'autocomplete' => $this->autocomplete() === false ? 'off' : 'on',
+            'autofocus'    => $this->autofocus(),
+            'placeholder'  => $this->i18n($this->placeholder()),
+            'readonly'     => $this->readonly(),
+            'disabled'     => $this->disabled(),
+            'id'           => $this->id()
+        ]);
+
+        $input->html($this->value() ? htmlentities($this->value(), ENT_NOQUOTES, 'UTF-8') : false);
+
+        if ( $this->readonly() ) {
+            $input->attr('tabindex', '-1');
+            $input->addClass('input-is-readonly');
+        }
 
         $wrapper = new Brick('div', false);
         $wrapper->append($input);
-        $wrapper->append('<script>tinymce.init({ selector: "#mytextarea" });</script>');
+        $wrapper->append('<script>tinymce.init({ selector: "textarea.field-tinymce" });</script>');
         return $wrapper;
     }
 
